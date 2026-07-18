e9e5b260eee8f53d8555918db90d9736437c85d9
Merctxt <giovannihettwer2@gmail.com>
2026-07-17 23:35:59 -0300

    configure https to reverse proxy

--- EaaSAPI/Program.cs
+++ EaaSAPI/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.HttpOverrides;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +9,18 @@ builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                               ForwardedHeaders.XForwardedProto |
+                               ForwardedHeaders.XForwardedHost;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 app.MapOpenApi();
@@ -18,7 +29,9 @@ app.MapScalarApiReference(options =>
 {
     options.WithTitle("SMTP API")
            .ForceDarkMode()
-           .WithTheme(ScalarTheme.DeepSpace);
+           .WithTheme(ScalarTheme.DeepSpace)
+           .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+           .AddPreferredSecuritySchemes("https");
 });
 
 app.UseAuthorization();
